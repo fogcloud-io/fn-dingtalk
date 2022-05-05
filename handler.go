5bbd61572f2d47b0d5e025d0121fb3bad026a7da
zhw <eternalzhw@gmail.com>
2022-05-05 16:43:45 +0800

    refactor: 💡 fission function

--- handler.go
+++ handler.go
@@ -1,9 +1,11 @@
-package function
+package main
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -20,22 +22,29 @@ type DingtalkMsg struct {
 	} `json:"text"`
 }
 
-// Handle a serverless request
-func Handle(req []byte) string {
+func Handler(w http.ResponseWriter, r *http.Request) {
+	body := r.Body
+	reqBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		log.Printf("ioutil.ReadAll: %s", err)
+		http.Error(w, "ioutil.ReadAll: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 	msg := DingtalkMsg{
 		MsgType: "text",
 		Text: struct {
 			Content string "json:\"content\""
 		}{
-			Content: string(req),
+			Content: string(reqBytes),
 		},
 	}
 
 	respData, err := sendDingtalkMsg(accessToken, &msg)
 	if err != nil {
-		return fmt.Sprintf("sendDingtalkMsg: %s", err)
+		log.Printf("sendDingtalkMsg: %s", err)
+		http.Error(w, "sendDingtalkMsg: "+err.Error(), http.StatusInternalServerError)
 	} else {
-		return string(respData)
+		w.Write(respData)
 	}
 }
 
@@ -45,5 +54,8 @@ func sendDingtalkMsg(accessToken string, msg *DingtalkMsg) (respData []byte, err
 	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("%s?access_token=%s", dingtalkURL, accessToken), buf)
 	req.Header.Set("Content-Type", "application/json")
 	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("sendDingtalkMsg: %s", err)
+	}
 	return
 }
